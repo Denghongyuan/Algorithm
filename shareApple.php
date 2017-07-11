0bd25d6b5c90859bad88083b4f2be15a0d414611
Denghongyuan <244906647@qq.com>
2017-07-11 23:00:12 +0800

    添加分赃苹果两种求总方法

--- shareApple.php
+++ shareApple.php
@@ -5,6 +5,65 @@
  * 二天，大家分赃，也是分成5份多一个扔给猴了。最后一人分了一份。问：共有多少苹果？
  */
 
+function appleShare()
+{
+	$avarage = 1;
+	$total = $avarage * 5;
+	$times = 0;
+	$share_times = 1;
+	while ($share_times < 6) {
+		if (is_int($total * 5/4)) {
+			$total = $total*5/4 +1;
+			$share_times++;
+		} else {
+			$share_times = 1;
+			$avarage++;
+			$total = $avarage * 5;
+		}
+		$times++;
+	}
+	echo 'query_times : '.$times.'<br>';
+	echo 'total apples : '.$total;
+}
 
+function appleShare2()
+{
+	for ($i = 1; ; $i++)
+	{
+	    if ($i%5 == 1) {
+	        //第一个人取五分之一，还剩$t
+	        $t = $i - round($i/5) - 1;      
+	        if($t % 5 == 1)
+	        {
+	            //第二个人取五分之一，还剩$r
+	            $r = $t - round($t/5) - 1;      
+	            if($r % 5 == 1)
+	            {
+	                //第三个人取五分之一，还剩$s
+	                $s = $r - round($r/5) - 1;              
+	                if($s % 5 == 1)
+	                {
+	                    //第四个人取五分之一，还剩$x
+	                    $x = $s - round($s/5) - 1;                  
+	                    if($x % 5 == 1)
+	                    {
+	                        //第五个人取五分之一，还剩$y
+	                        $y = $x - round($x/5) - 1;                      
+	                        if ($y % 5 == 0) {
+	                            echo 'total apples : '.$i;
+	                            break;
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    }
+	}
+}
+
+
+appleShare();
+echo '<br>';
+appleShare2();;
 
 ?>
